173bdb55ae51773bd56ff7412a5c1bdb37e59a0f
your_name <your_email@example.com>
2020-12-01 12:38:48 -0500

    commit tree c983489f336f385968d3ccfbe081260782b37c80

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+       "My name is Zanny" 
+    end 
+
+    get '/hometown' do 
+        "My hometown is Woodbridge"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Rick Roll"
+    end 
 end
